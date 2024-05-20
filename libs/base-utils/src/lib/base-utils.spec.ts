ef67a8aba044587327055d6b9ff30f7d5cecacc7
Moritz Hipper <moritz.hipper@outlook.de>
2024-05-20 16:33:01 +0200

    added shared lib

---
+++ libs/base-utils/src/lib/base-utils.spec.ts
@@ -0,0 +1,7 @@
+import { baseUtils } from './base-utils';
+
+describe('baseUtils', () => {
+   it('should work', () => {
+      expect(baseUtils()).toEqual('base-utils');
+   });
+});
---
+++ libs/base-utils/src/lib/base-utils.ts
@@ -0,0 +1,3 @@
+export function baseUtils(): string {
+   return 'base-utils';
+}
