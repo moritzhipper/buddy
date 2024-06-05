16b0f19a114c862cbe7fe9060489f9cf0a45f2b5
Moritz Hipper <moritz.hipper@outlook.de>
2024-06-05 22:32:58 +0200

    feat: also added initial desktop design

--- apps/frontend/src/app/components/pages/search/search-results/search-results.component.ts
+++ apps/frontend/src/app/components/pages/search/search-results/search-results.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common'
-import { Component, Input } from '@angular/core'
+import { Component, Input, inject } from '@angular/core'
 import { Therapist } from '@buddy/base-utils'
+import { Store } from '@ngrx/store'
+import { InputResolveTypes, InputService, InputTypes } from 'apps/frontend/src/app/services/input.service'
+import { therapistActions } from 'apps/frontend/src/app/store/buddy.actions'
 
 @Component({
    selector: 'app-search-results',
@@ -12,8 +15,23 @@ import { Therapist } from '@buddy/base-utils'
 export class SearchResultsComponent {
    @Input() therapists: Therapist[]
 
-   addTherapist(therapist: Therapist) {
+   inputService = inject(InputService)
+   store = inject(Store)
+
+   async addTherapist(therapist: Therapist) {
       //hier popup 'Therapeut übernehmen?'
+      const wantsToSaveResponse = await this.inputService.openInputDialogue({
+         type: InputTypes.CONFIRM,
+         header: 'Person speichern',
+         description: `Möchtest du ${therapist.name} in deine persönliche Liste übernehmen?`,
+      })
+
+      if (wantsToSaveResponse.type === InputResolveTypes.CONFIRM) {
+         const { id } = therapist
+
+         this.store.dispatch(therapistActions.create({ props: { id } }))
+      }
+
       console.log(therapist.name, therapist.id)
    }
 }
